9c9ee0f439d7ee991b79ea4256d2a13d23e86e1b
Chris Warburton <chriswarbo@gmail.com>
2016-12-26 20:53:34 +0000

    More tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,29 +1,37 @@
-{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings, GADTs #-}
 
 module Main where
 
-import Control.Applicative
-import Data.Char
-import Data.List
-import SampleBench hiding (defaultMain)
-import System.Environment
-import Test.QuickCheck
-import Test.QuickCheck.Monadic
-import Test.Tasty (defaultMain, testGroup, localOption)
-import Test.Tasty.QuickCheck
+import           Control.Applicative
+import           Data.Aeson
+import qualified Data.ByteString.Char8      as BSC
+import qualified Data.ByteString.Lazy.Char8 as LBSC
+import           Data.Char
+import           Data.List
+import           SampleBench hiding (defaultMain)
+import           System.Environment
+import           Test.QuickCheck
+import           Test.QuickCheck.Monadic
+import           Test.Tasty (defaultMain, testGroup, localOption)
+import           Test.Tasty.QuickCheck
 
 main = defaultMain $ testGroup "All tests" [
-    testProperty "Can parse command" canGetBenchCommand
-  --, testProperty ""
+    testProperty "Can parse command"  canGetBenchCommand
+  , testProperty "JSON encode/decode" jsonDecode
   ]
 
 -- Tests
+jsonDecode :: NonEmptyString -> NonEmptyString -> Property
+jsonDecode (NES name) val = '=' `notElem` name ==> monadicIO $ do
+  val' <- run $ withEnv [(name, show (JSON val))]
+                        (jsonFromVar name)
+  assert (val == val')
 
-canGetBenchCommand (JS s) = monadicIO $ do
-  s' <- run $ withEnv [("BENCHMARK_COMMAND", show s)] getBenchCommand
+canGetBenchCommand (NES s) = monadicIO $ do
+  s' <- run $ withEnv [("BENCHMARK_COMMAND", show (JSON s))] getBenchCommand
   assert (s == s')
 
--- Helper functions
+-- Helpers
 
 withEnv :: [(String, String)] -> IO a -> IO a
 withEnv []               p = p
@@ -41,17 +49,35 @@ withEnv ((name, val):xs) p = do
        Just old -> setEnv name old
   return result
 
-newtype JsonString = JS String
+newtype NonEmptyString = NES String deriving (Eq)
 
-instance Arbitrary JsonString where
-  arbitrary = JS <$> nonEmpty
-    where nonEmpty = ascii `suchThat` (not . null)
-          ascii    = filter valid <$> listOf1 arbitrary
+instance Show NonEmptyString where
+  show (NES s) = s
+
+instance ToJSON NonEmptyString where
+  toJSON (NES s) = toJSON s
+
+instance FromJSON NonEmptyString where
+  parseJSON x = NES <$> parseJSON x
+
+instance Arbitrary NonEmptyString where
+  arbitrary = NES <$> listOf1 ascii
+    where ascii    = arbitrary `suchThat` valid
           valid x  = isPrint x && isAscii x
-  shrink (JS s) = case s of
+  shrink (NES s) = case s of
+    ""     -> error "Empty NonEmptyString"
     "a"    -> []
-    (_:[]) -> [JS "a"]
-    (_:cs) -> map JS (tails cs)
+    (_:[]) -> [NES "a"]
+    _      -> let hs = tail          (init (inits s))
+                  ts = tail (reverse (tail (tails s)))
+               in map NES (hs ++ ts)
 
-instance Show JsonString where
-  show (JS s) = show s
+data JsonOf a where
+  JSON :: (FromJSON a, ToJSON a) => a -> JsonOf a
+
+instance (Arbitrary a, ToJSON a, FromJSON a) => Arbitrary (JsonOf a) where
+  arbitrary = JSON <$> arbitrary
+  shrink (JSON s) = map JSON (shrink s)
+
+instance (Show a) => Show (JsonOf a) where
+  show (JSON s) = LBSC.unpack (encode s)

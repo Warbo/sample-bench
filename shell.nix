00a632a6fe635676cc32b7fa4312957f5393dd9a
Chris Warburton <chriswarbo@gmail.com>
2017-02-10 00:36:18 +0000

    Clean slate

--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-with builtins;
-with {
-  call = haskell.packages.ghc7103.callPackage;
-};
-haskell.packages.ghc7103.ghcWithPackages (hs: [
-  (hs.callPackage (runCabal2nix { url = ./.; }) {})
-  (hs.callPackage (runCabal2nix { url = "cabal://criterion-0.8.1.0"; }) {})
-])

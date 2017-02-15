8d31ac4b460da57330ab15765fff102ea545ddb4
Chris Warburton <chriswarbo@gmail.com>
2017-02-15 04:39:02 +0000

    Fixes

--- default.nix
+++ default.nix
@@ -65,7 +65,7 @@ with rec {
     paths = [ jq usableNix time timeout ];
   };
 
-  fail = msg: "{ echo -e '${msg}'; exit 1; }";
+  fail = msg: ''{ echo -e "${msg}"; exit 1; }'';
 };
 stdenv.mkDerivation {
   name         = "sample-bench";
@@ -74,10 +74,52 @@ stdenv.mkDerivation {
 
   doCheck    = true;
   checkPhase = ''
+    printf "Testing that benchmark needs arguments" 1>&2
     ./benchmark 2> /dev/null && ${fail "No args should fail"}
-    RESULT=$(CMD="echo" GEN_INPUT="echo" INFO="x y z" REPS=3 ./benchmark) ||
-      ${fail "benchmark died"}
+
+    echo "Testing benchmark of simple command" 1>&2
+    RESULT=$(      CMD="echo"  \
+             GEN_INPUT="echo"  \
+                  INFO="x y z" \
+                  REPS=3 ./benchmark) || ${fail "benchmark died"}
     echo "$RESULT" | jq '.' > /dev/null || ${fail "jq rejected our output"}
+
+    echo "Testing that benchmark times out" 1>&2
+    RESULT=$(GEN_INPUT="true"  \
+                   CMD="${writeScript "tst" ''
+                            #!/usr/bin/env bash
+                            while true; do true; done
+                          ''}" \
+                  INFO=        \
+                  REPS=2       \
+              MAX_SECS=3 ./benchmark 2> stderr) ||
+      ${fail "Couldn't bench with timeout\n$(cat stderr)"}
+    ERR=$(cat stderr)
+
+    L=$(echo "$RESULT" | jq '.results | length') ||
+      ${fail "Couldn't get length"}
+    [[ "$L" -eq 2 ]] || ${fail "Didn't get 2 reps:\n$RESULT\n$ERR"}
+
+    F=$(echo "$RESULT" | jq '.results | map(.failure != null) | all') ||
+      ${fail "Couldn't get failures"}
+    [[ "x$F" = "xtrue" ]] || ${fail "Should have failed\n$ERR"}
+
+    echo "Testing that benchmark memory usage is capped" 1>&2
+    RESULT=$(GEN_INPUT="true"  \
+                   CMD="${writeScript "tst" ''
+                            #!/usr/bin/env bash
+                            S="x"
+                            while true; do X="$X$X"; done
+                          ''}" \
+                  INFO=        \
+                  REPS=2       \
+                MAX_KB=1000 ./benchmark 2> stderr) ||
+      ${fail "Couldn't limit memory\n$(cat stderr)"}
+
+    ERR=$(cat stderr)
+    F=$(echo "$RESULT" | jq '.results | map(.failure != null) | all') ||
+      ${fail "Couldn't get failures\n$ERR"}
+    [[ "x$F" = "xtrue" ]] || ${fail "Should have failed\n$ERR"}
   '';
 
   installPhase = ''
